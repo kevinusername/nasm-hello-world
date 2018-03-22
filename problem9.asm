f1935e74e13156a54341767db1a51f8975b7b26e
Kevin Peters <35283725+kpeter51@users.noreply.github.com>
2018-03-21 21:54:04 -0700

    Initial working version

---
+++ problem9.asm
@@ -0,0 +1,15 @@
+    global  _start
+
+    section	.text
+_start:
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, message
+    mov     rdx, 19
+    syscall
+    mov     rax, 60
+    xor     rdi, rdi
+    syscall
+
+    section .data
+message:	db	"最近怎么样？", 10
